ad53a06e0208d4d2bd56d8af183ee37f4525837a
ADAM VARDEN - STUDENT <G00359605@gmit.ie>
2019-10-03 14:36:15 +0100

    first commit

---
+++ src/components/create.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+
+class Create extends React.Component {
+ render(){
+    return (
+      <div className="App">
+      <h3 >Create component</h3>
+      </div>
+    );
+  }
+}
+export default Create;
